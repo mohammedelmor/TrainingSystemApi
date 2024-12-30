499bdf279f241ae2071efae36d8c0467f5c039e2
dpp500011 <M.Atef@dxbpp.gov.ae>
2024-12-30 12:44:15 +0400

    Initial Commit

---
+++ src/main/java/org/elmor/trainingsystemapi/TrainingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package org.elmor.trainingsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/elmor/trainingsystemapi/TrainingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elmor.trainingsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
